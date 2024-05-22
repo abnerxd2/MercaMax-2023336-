331911876800e61ff9963240782502e04de236a4
abner kinal <abnerrivas848@gmail.com>
2024-05-22 04:11:56 -0600

    Subo mas vistas y controllers

---
+++ src/org/abnerdelcid/controller/MenuCargoEmpleadoController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.abnerdelcid.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author abner
+ */
+public class MenuCargoEmpleadoController implements Initializable {
+
+    @FXML
+    private Button btnAgregar;
+    @FXML
+    private Button btnEliminar;
+    @FXML
+    private Button btnEditar;
+    @FXML
+    private Button btnReporte;
+    @FXML
+    private TextField txtCodigoCP;
+    @FXML
+    private TextField txtNombreCP;
+    @FXML
+    private TextField txtDescrimpcionCP;
+    @FXML
+    private TableView<?> tblCargoEmpleado;
+    @FXML
+    private TableColumn<?, ?> colCodigoCP;
+    @FXML
+    private TableColumn<?, ?> colNombreCP;
+    @FXML
+    private TableColumn<?, ?> colDescripcionCp;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
